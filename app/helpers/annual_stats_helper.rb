255b45ae9399a9220841622b1b27b756d164a912
Paul Nelson <panel@umich.edu>
2013-03-03 09:02:04 -0600

    handles when stats are null

--- app/helpers/annual_stats_helper.rb
+++ app/helpers/annual_stats_helper.rb
@@ -1,7 +1,7 @@
 module AnnualStatsHelper
   def stat_with_rank(stat,o, percent = false, precision = 2)
     stat = stat.to_sym if stat.is_a? String
-    if !stat.nil?
+    if !o.send(stat).nil?
       string = (o.send(stat).round(precision)).to_s
       string+= '%' if percent == true
       string+= " (#{o.ranks[stat]})"
