6a760328fe117c084fd733c46df10ca9cdf1a24d
Paul Nelson <panel@umich.edu>
2013-03-03 03:20:11 -0600

    view change

--- app/helpers/annual_stats_helper.rb
+++ app/helpers/annual_stats_helper.rb
@@ -1,9 +1,10 @@
 module AnnualStatsHelper
   def stat_with_rank(stat,o, percent = false, precision = 2)
     stat = stat.to_sym if stat.is_a? String
-    
-    string = (o.send(stat).round(precision)).to_s
-    string+= '%' if percent == true
-    string+= " (#{o.ranks[stat]})"
+    if stat.is_a? Float
+      string = (o.send(stat).round(precision)).to_s
+      string+= '%' if percent == true
+      string+= " (#{o.ranks[stat]})"
+    end  
   end
 end
