af5f1d873ef5024d91bfe09070ae967738a16b97
Paul Nelson <panel@umich.edu>
2013-03-17 15:15:36 -0500

    fixed floating header and added handling for errors in summing player stats

--- app/models/player_annual_stat.rb
+++ app/models/player_annual_stat.rb
@@ -15,9 +15,13 @@ class PlayerAnnualStat < ActiveRecord::Base
     pgs = PlayerGameStat.joins(:player, :game).where('games.date' => range).select(:player_id).uniq
     pgs.each do |t|
       if t.player.team_id != 65
-        stat = PlayerAnnualStat.find_or_create_by_player_id_and_year(:player_id => t.player_id, :year => Date.new(year,1,1))
-        stat.sum
-        puts t.player_id
+        begin
+          stat = PlayerAnnualStat.find_or_create_by_player_id_and_year(:player_id => t.player_id, :year => Date.new(year,1,1))
+          stat.sum
+          puts t.player_id
+        rescue Exception => e
+          puts "Unable to sum for player: #{t.player.id}\n#{e.inspect}"
+        end
       end
     end
   end
